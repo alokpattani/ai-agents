35109c18f41877358257c0bfa77795c286238ffb
Alok Pattani <51244947+alokpattani@users.noreply.github.com>
2025-05-28 14:55:32 -0700

    Add files via upload

--- ice_cream_agent/agent.py
+++ ice_cream_agent/agent.py
@@ -1,12 +1,10 @@
 from google.adk.agents import Agent
-from google.adk.tools import google_search, agent_tool, FunctionTool, ToolContext
-from typing import Dict
+from google.adk.tools import google_search, FunctionTool, ToolContext
 
 import os
 from dotenv import load_dotenv
 
 from google import genai
-
 from google.genai.types import (
     ApiKeyConfig,
     AuthConfig,
@@ -24,33 +22,25 @@ GOOGLE_CLOUD_PROJECT = os.getenv('GOOGLE_CLOUD_PROJECT')
 GOOGLE_CLOUD_LOCATION = os.getenv('GOOGLE_CLOUD_LOCATION') 
 GOOGLE_MAPS_GROUNDING_API_KEY = os.getenv('GOOGLE_MAPS_GROUNDING_API_KEY')
 ICE_CREAM_PLACES_INFO_GCS_URI = os.getenv('ICE_CREAM_PLACES_INFO_GCS_URI')
+GEMINI_MODEL_ID = os.getenv('GEMINI_MODEL_ID')
 
 client = genai.Client(vertexai=GOOGLE_GENAI_USE_VERTEXAI, project=GOOGLE_CLOUD_PROJECT, 
     location=GOOGLE_CLOUD_LOCATION)
 
-ice_cream_places_search_agent = Agent(
-    name="ice_cream_places_search_agent",
-    model="gemini-2.5-flash-preview-04-17",
-    description="Agent to search for information about ice cream places using Google Search.",
-    instruction="I can find information about real ice cream places using Google Search.",
-    tools=[google_search]
-    )
 
-def get_my_ice_cream_places_info(prompt: str, tool_context: ToolContext) -> str:
-    """Returns info about my ice cream places from CSV file containing reviews, ratings,
-        and other information about the ice cream places that I've been to and documented
+def get_aloks_ice_cream_places_info(prompt: str, tool_context: ToolContext) -> str:
+    """Returns info about Alok's ice cream places from CSV file containing favorite flavors, ratings,
+        and other things about the ice cream places that Alok has been to and documented
 
     Args:
-        prompt (str): The prompt related to getting info about my ice cream places
+        prompt (str): The prompt related to getting info about Alok's ice cream places
 
     Returns:
-        str: Gemini text response grounded in my ice cream places info
+        str: Gemini text response grounded in Alok's ice cream places info
     """
 
-    MY_ICE_CREAM_PLACES_MODEL_ID = "gemini-2.5-flash-preview-04-17"
-
-    my_ice_cream_places_generation_response = client.models.generate_content(
-        model=MY_ICE_CREAM_PLACES_MODEL_ID,
+    aloks_ice_cream_places_generation_response = client.models.generate_content(
+        model=GEMINI_MODEL_ID,
         contents=Content(
             role="user",
             parts=[Part.from_text(text=prompt)] + 
@@ -58,27 +48,28 @@ def get_my_ice_cream_places_info(prompt: str, tool_context: ToolContext) -> str:
             ),
         config=GenerateContentConfig(
             system_instruction=(
-                "You are an ice cream expert with knowledge of various ice cream places that "
-                "I've been to, including my personal ratings, favorite flavors, and other notes. "
-                "Please only use the provided reference material to respond about inquiries "
-                "related to ice cream places. If prompts come in about ice cream places I have " 
-                "not been to, respond with 'I don't have personal experience with that place.'"
+                "You are an ice cream expert with knowledge of various ice cream places that Alok "
+                "has been to, including his personal ratings, favorite flavors, and other notes. "
+                "Please only use the provided reference material to respond about inquiries related "
+                "to ice cream places. If prompts come in about ice cream places Alok has not been to, " 
+                "respond that Alok hasn't been to that place (or any places in that area)."
                 )
         )
     )
 
-    return(my_ice_cream_places_generation_response.text)
+    return aloks_ice_cream_places_generation_response.text
 
 
 def find_ice_cream_places(location: str, tool_context: ToolContext) -> str:
-    """Returns nearby ice cream places for a specific location using Gemini 
+    """Returns information about ice cream places for a specific location using Gemini 
     with grounding in Google Maps
 
     Args:
-        location (str): The location for which to find ice cream places nearby
+        location (str): The location for which to find ice cream places (or information
+           about them) nearby
 
     Returns:
-        str: Gemini text response about nearest ice cream places
+        str: Gemini text response about ice cream places near the location in question
     """
 
     google_maps_tool = Tool(
@@ -91,12 +82,10 @@ def find_ice_cream_places(location: str, tool_context: ToolContext) -> str:
         )
     )
 
-    MAPS_GROUNDING_MODEL_ID = "gemini-2.5-flash-preview-04-17"
-
     response = client.models.generate_content(
-        model=MAPS_GROUNDING_MODEL_ID,
+        model=GEMINI_MODEL_ID,
         contents=[
-            f"Find ice cream places in or near {location}.",
+            f"Find information about ice cream places in or near {location}.",
             ],
         config=GenerateContentConfig(
             system_instruction=(
@@ -110,25 +99,26 @@ def find_ice_cream_places(location: str, tool_context: ToolContext) -> str:
 
     return response.text
 
+
 ice_cream_agent = Agent(
     name="ice_cream_agent",
-    model="gemini-2.5-flash-preview-04-17",
+    model=GEMINI_MODEL_ID,
     description=(
-        "Ice cream agent to find ice cream places and get information about them from Google Maps, "
-        "Google Search, and my personal reviews and ratings."
+        "Ice cream agent to find ice cream places and get information about them from Google Maps "
+        "and Alok's personal reviews and ratings."
     ),
     instruction=(
         "You are an ice cream agent that can find ice cream places across the world and relevant "
-        "information about those places from Google Maps, Google Search, and my personal reviews "
-        "and ratings. Choose the appropriate tool to respond to queries about ice cream places, "
-        "trying to leverage my personal info when it's available. Feel free to use mutiple tools "
-        "when responding to queries (e.g. get top ice cream places in a particular location from "
-        "both Google Maps and my personal history)."
+        "information about those places from Google Maps and Alok's personal reviews and ratings. "
+        "Choose the appropriate tool to respond to queries about ice cream places, trying to "
+        "leverage Alok's personal info when it's available. Feel free to use both tools when "
+        "responding to queries where it makes sense to have both the big-picture public perspective "
+        "from Google Maps and Alok's personal curated experience (e.g. get top ice cream places in "
+        "particular location from both Google Maps and Alok's personal history)."
     ),
     tools=[
-        agent_tool.AgentTool(agent=ice_cream_places_search_agent),
         find_ice_cream_places,
-        get_my_ice_cream_places_info
+        get_aloks_ice_cream_places_info
         ]
     )
 
